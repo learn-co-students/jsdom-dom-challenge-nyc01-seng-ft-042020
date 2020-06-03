0b6c48e18b31e133d0682d435afe48b4cb6b9acd
carolineglass <carolineglass89@gmail.com>
2020-06-03 15:51:57 -0400

    everything but resume

--- js/challenge.js
+++ js/challenge.js
@@ -25,14 +25,12 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     };
 
-    // setInterval(increment, 1000);
+    const t = setInterval(increment, 1000);
 
-    const not_paused = true
-
-    // while (not_paused) {
-    //     console.log("incrementing!");
-    //     setInterval(increment, 1000);
-    // };
+    const buttons =
+            [document.getElementById('minus'),
+            document.getElementById('plus'),
+            document.getElementById('heart')]
 
     document.addEventListener("click", function(e){
         if (e.target.id === "plus") {
@@ -49,8 +47,8 @@ document.addEventListener("DOMContentLoaded", function(){
         }
 
         else if (e.target.id === "pause") {
-            not_paused = false;
-            // clearTimeout(setInterval(increment, 1000)); 
+            clearTimeout(t); 
+            buttons.forEach(element => element.disabled = true)
             // console.log("pausing");
 
             e.target.textContent = "resume";
@@ -58,10 +56,22 @@ document.addEventListener("DOMContentLoaded", function(){
         }
 
         else if (e.target.id === "resume") {
-            not_paused = true;
-
+            // setInterval(increment, 1000);
+            buttons.forEach(element => element.disabled = false)
             e.target.textContent = "pause";
             e.target.id = "pause";
         }
     })
+
+    document.getElementById('comment-form').addEventListener("submit", function(e) {
+        const list = document.querySelector('#list')
+        const li = document.createElement('p')
+        list.appendChild(li)
+        const textBox = document.querySelector("#comment-input")
+        li.textContent = textBox.value
+        e.preventDefault()
+        console.log(textBox.value)
+    })
+    
 });
+
