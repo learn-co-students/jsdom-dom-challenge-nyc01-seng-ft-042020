e6cce33fcf0fd79ca67495d1289803cdcb58ebbd
blobbyblobfish <aywang6@gmail.com>
2020-06-03 17:35:34 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const counter = document.querySelector('#counter')
 
-    const likes = document.querySelector(".likes")
+    const counter = document.querySelector('#counter');
 
-    let clicks = 0;
+    const likes = document.querySelector(".likes");
+
+    let timer = setInterval(increment, 1000);
+
+    let isTimerRunning = true;
+
+    const buttons =
+            [document.getElementById('minus'),
+            document.getElementById('plus'),
+            document.getElementById('heart')];
 
     function increment () {
         counter.textContent = parseInt(counter.textContent) + 1
@@ -13,29 +21,43 @@ document.addEventListener("DOMContentLoaded", function(){
         counter.textContent = parseInt(counter.textContent) - 1
     };
 
-    function like(clicks){
-        if (clicks === 0) {
+    function like(){
+
+        liExists = document.getElementById(`${counter.innerText}`)
+
+        if (liExists) {
+            liExists.dataset.likes++
+            liExists.textContent = `${counter.textContent} has been liked ${liExists.dataset.likes} times`;
+        }
+
+        else {
             const newLi = document.createElement("li");
+            newLi.id = `${counter.innerText}`
+            newLi.dataset.likes = 1;
+            newLi.textContent = `${counter.textContent} has been liked ${newLi.dataset.likes} times`;
             likes.appendChild(newLi);
-            clicks++;
-            newLi.textContent = `${counter.textContent} has been liked ${clicks} times`;
         }
-        else if (clicks > 0) {
-            likes.querySelector("li").textContent = `${counter.textContent} has been liked ${clicks} times`;
-        }
+
+        // console.log(clicks);
+
+        // if (clicks === 0) {
+        //     const newLi = document.createElement("li");
+        //     likes.appendChild(newLi);
+        //     clicks++;
+        //     console.log(clicks);
+        //     newLi.textContent = `${counter.textContent} has been liked ${clicks} times`;
+        // }
+
+        // else if (clicks > 0) {
+        //     clicks++;
+        //     console.log(clicks);
+        //     likes.querySelector("li").textContent = `${counter.textContent} has been liked ${clicks} times`;
+        // }
+
     };
-
-    const t = setInterval(increment, 1000);
-
-    const buttons =
-            [document.getElementById('minus'),
-            document.getElementById('plus'),
-            document.getElementById('heart')]
 
     document.addEventListener("click", function(e){
         if (e.target.id === "plus") {
-            // const body = e.target.parentNode
-            // const counter = body.getElementById("counter")
             increment();
         }
         else if (e.target.id === "minus") {
@@ -43,34 +65,34 @@ document.addEventListener("DOMContentLoaded", function(){
         }
 
         else if (e.target.id === "heart") {
-            like(clicks);
+            like();
         }
 
         else if (e.target.id === "pause") {
-            clearTimeout(t); 
-            buttons.forEach(element => element.disabled = true)
-            // console.log("pausing");
+            if (isTimerRunning) {
+                clearInterval(timer); 
+                buttons.forEach(element => element.disabled = true);
+                e.target.textContent = "resume";
+                isTimerRunning = false 
+            }
 
-            e.target.textContent = "resume";
-            e.target.id = "resume";
-        }
-
-        else if (e.target.id === "resume") {
-            // setInterval(increment, 1000);
-            buttons.forEach(element => element.disabled = false)
-            e.target.textContent = "pause";
-            e.target.id = "pause";
+            else {
+                timer = setInterval(increment, 1000)    
+                buttons.forEach(element => element.disabled = false);
+                e.target.textContent = "pause";
+                isTimerRunning = true 
+            }
         }
     })
 
     document.getElementById('comment-form').addEventListener("submit", function(e) {
+        const textBox = document.querySelector("#comment-input")
         const list = document.querySelector('#list')
         const li = document.createElement('p')
         list.appendChild(li)
-        const textBox = document.querySelector("#comment-input")
+
         li.textContent = textBox.value
         e.preventDefault()
-        console.log(textBox.value)
     })
     
 });
